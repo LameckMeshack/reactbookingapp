33d03210c3dafedfa72f3c4eaf2541745a734dc2
Rhinolamer <rhinolamer@gmail.com>
2022-05-08 16:21:26 +0300

    starterted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+
 function App() {
   return (
-    <div>
-      Hello world!
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
