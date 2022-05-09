6205603fe2d1522736504a163bef17bd8d1f3c48
Rhinolamer <rhinolamer@gmail.com>
2022-05-09 16:06:53 +0300

    homepage list designed

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,45 @@
+import "./featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Nairobi</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+
+      <div className="featuredItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Mombasa</h1>
+          <h2>533 properties</h2>
+        </div>
+      </div>
+
+      <div className="featuredItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Nakuru</h1>
+          <h2>532 properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
